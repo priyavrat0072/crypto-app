9d98568e3c7031233dc4b594cb39c79968bde547
Priyavrat Singh Chouhan <priyavrat0072@gmail.com>
2023-08-11 16:44:16 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import Header from './component/Header';
+import Home from './component/Home';
+import CoinDetails from './component/CoinDetails';
+import Coins from './component/Coins';
+import Exchange from './component/Exchange';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>CRYPTO APP</h1>
-    </div>
+    <>
+   
+   <Router>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/coins' element={<Coins/>} />
+      <Route path='/exchange' element={<Exchange/>} />
+      <Route path='/coin/:id' element={<CoinDetails/>} />
+    </Routes>
+   </Router>
+   </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ChakraProvider, theme} from '@chakra-ui/react'
 
 import App from './App';
 
@@ -7,7 +8,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider>
+   
   </React.StrictMode>
 );
 
